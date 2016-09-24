b1832c98942a7d149540e76b3a8b8c4ac864f810
shaneyake <shaneyake2@gmail.com>
2016-09-24 13:08:26 -0400

    Update dashboard.php

--- HAC.-7-MQTT-to-Webpages/html/dashboard.php
+++ HAC.-7-MQTT-to-Webpages/html/dashboard.php
@@ -3,11 +3,11 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Dashboard</title>
+<title>Control</title>
 <script language="javascript" type="text/javascript">
   function init()
   {
-	doConnect();
+        doConnect();
   }
   function doConnect()
   {
@@ -19,85 +19,69 @@
   }
   function onOpen(evt)
   {
-    writeToScreen("connected\n");
+    console.log("connected\n");
   }
   function onClose(evt)
   {
-    writeToScreen("disconnected\n");
+    console.log("disconnected\n");
   }
   function onMessage(evt)
   {
     console.log(evt.data);
-	writeToScreen("response: " + evt.data + '\n');
-	var obj = JSON.parse(evt.data);
+        var obj = JSON.parse(evt.data);
     if(obj['mode']=="login"){
-		doSend('{"mode": "login", "ws_token":"<?php echo $_SESSION["WS_TOKEN"]; ?>"}');
-	}else{
-	    
-	}
+                  websocket.send('{"mode": "login", "ws_token":"<?php echo $_SESSION["WS_TOKEN"]; ?>"}');
+          }else{
+            
+          }
   }
   function onError(evt)
   {
-    writeToScreen('error: ' + evt.data + '\n');
-	websocket.close();
-  }
-  function doSend(message)
-  {
-    writeToScreen("sent: " + message + '\n'); 
-    websocket.send(message);
-  }
-  function writeToScreen(message)
-  {
-    document.myform.outputtext.value += message
-	document.myform.outputtext.scrollTop = document.myform.outputtext.scrollHeight;
+    console.log("ERROR");
+          websocket.close();
   }
   window.addEventListener("load", init, false);
   function sendSub() {
        var array_temp={};
        array_temp['mode']="subscribe";
        array_temp['topic']=document.myform.inputtextSub.value;
-       doSend(JSON.stringify(array_temp));
+       websocket.send(JSON.stringify(array_temp));
    }
-   function sendPub() {
+   function sendPub(topic,message) {
        var array_temp={};
        array_temp['mode']="publish";
-       array_temp['topic']=document.myform.inputtextPubTopic.value ;
-       array_temp['message']=document.myform.inputtextPub.value ;
-       doSend(JSON.stringify(array_temp));
+       array_temp['topic']=topic;
+       array_temp['message']=message;
+       websocket.send(JSON.stringify(array_temp));
    }
-  function clearText() {
-		document.myform.outputtext.value = "";
+
+   function sendRGB(color,value){
+       var array_temp={};
+       array_temp[color]=value;
+       sendPub('hello/world',array_temp);
    }
-   function doDisconnect() {
-		websocket.close();
+
+   function sendButtonPress(value){
+       var array_temp={};
+       array_temp['STATE']=value;
+       sendPub('hello/world',array_temp);
    }
 </script>
 </head>
 
 <body>
-Dashboard<br>
+Control<br>
 <a href="/?p=logout">Log Out</a>
-<div id="output"></div>
+<br>
 
-<form name="myform">
-<p>
-<textarea name="outputtext" rows="20" cols="100"></textarea>
-</p>
-Subscribe
-<p>
-<textarea name="inputtextSub" cols="50"></textarea><input type="button" name=sendButton value="Send" onClick="sendSub();" style="height: 36px;    top: -14px;    position: relative;">
-</p>
-Publish
-<p>
-<textarea name="inputtextPubTopic" cols="50"></textarea><textarea name="inputtextPub" cols="50"></textarea><input type="button" name=sendButton value="Send" onClick="sendPub();" style="height: 36px;    top: -14px;    position: relative;">
-</p>
-<p>
-<input type="button" name=clearButton value="Clear" onClick="clearText();">
-<input type="button" name=disconnectButton value="Disconnect" onClick="doDisconnect();">
-<input type="button" name=connectButton value="Connect" onClick="doConnect();">
-</p>
+<br>
+RED___:<input type="range" id="myRange" value="90" min="0" max="255" onChange="sendRGB('RED',this.value)"><br>
+GREEN:<input type="range" id="myRange" value="90" min="0" max="255" onChange="sendRGB('GREEN',this.value)"><br>
+BLUE__:<input type="range" id="myRange" value="90" min="0" max="255" onChange="sendRGB('BLUE',this.value)"><br>
+<br>
+<button onClick="sendButtonPress(1)">ON</button><br>
+<button onClick="sendButtonPress(0)">OFF</button><br>
+<button onClick="sendButtonPress('T')">TOGGEL</button><br>
 
-
-</form>
 </body>
 </html>
