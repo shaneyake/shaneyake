ffb8f6f64e8634c8f13d88dc8d10c99ed2fa7880
shaneyake <shaneyake2@gmail.com>
2016-09-24 13:08:58 -0400

    Update control.php

--- HAC.-7-MQTT-to-Webpages-Part-2/control.php
+++ HAC.-7-MQTT-to-Webpages-Part-2/control.php
@@ -7,7 +7,7 @@
 <script language="javascript" type="text/javascript">
   function init()
   {
-	doConnect();
+        doConnect();
   }
   function doConnect()
   {
@@ -28,17 +28,17 @@
   function onMessage(evt)
   {
     console.log(evt.data);
-  	var obj = JSON.parse(evt.data);
+        var obj = JSON.parse(evt.data);
     if(obj['mode']=="login"){
-		  websocket.send('{"mode": "login", "ws_token":"<?php echo $_SESSION["WS_TOKEN"]; ?>"}');
-	  }else{
-	    
-	  }
+                  websocket.send('{"mode": "login", "ws_token":"<?php echo $_SESSION["WS_TOKEN"]; ?>"}');
+          }else{
+            
+          }
   }
   function onError(evt)
   {
     console.log("ERROR");
-	  websocket.close();
+          websocket.close();
   }
   window.addEventListener("load", init, false);
   function sendSub() {
@@ -47,12 +47,24 @@
        array_temp['topic']=document.myform.inputtextSub.value;
        websocket.send(JSON.stringify(array_temp));
    }
-   function sendPub() {
+   function sendPub(topic,message) {
        var array_temp={};
        array_temp['mode']="publish";
-       array_temp['topic']=document.myform.inputtextPubTopic.value ;
-       array_temp['message']=document.myform.inputtextPub.value ;
+       array_temp['topic']=topic;
+       array_temp['message']=message;
        websocket.send(JSON.stringify(array_temp));
+   }
+
+   function sendRGB(color,value){
+       var array_temp={};
+       array_temp[color]=value;
+       sendPub('hello/world',array_temp);
+   }
+
+   function sendButtonPress(value){
+       var array_temp={};
+       array_temp['STATE']=value;
+       sendPub('hello/world',array_temp);
    }
 </script>
 </head>
@@ -60,10 +72,16 @@
 <body>
 Control<br>
 <a href="/?p=logout">Log Out</a>
+<br>
 
+<br>
+RED___:<input type="range" id="myRange" value="90" min="0" max="255" onChange="sendRGB('RED',this.value)"><br>
+GREEN:<input type="range" id="myRange" value="90" min="0" max="255" onChange="sendRGB('GREEN',this.value)"><br>
+BLUE__:<input type="range" id="myRange" value="90" min="0" max="255" onChange="sendRGB('BLUE',this.value)"><br>
+<br>
+<button onClick="sendButtonPress(1)">ON</button><br>
+<button onClick="sendButtonPress(0)">OFF</button><br>
+<button onClick="sendButtonPress('T')">TOGGEL</button><br>
 
-
-
-</form>
 </body>
 </html>
